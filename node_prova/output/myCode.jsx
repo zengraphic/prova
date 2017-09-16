dd33049ef9e0df9302466eeab61f1817f77b83c6
Alessandro Napolitano <zengraphic@MacBook-Pro-di-Alessandro.local>
2017-09-16 23:19:56 +0200

    asd

---
+++ node_prova/output/myCode.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const prova = <h1>Hello</h1>;
+
+ReactDOM.render(prova,document.getElementById('container'));
---
+++ node_prova/webpack.config.js
@@ -0,0 +1,10 @@
+var webpack = require("webpack");
+var path = require("path");
+
+var DEV = path.resolve(__dirname, "dev");
+var OUTPUT = path.resolve(__dirname, "output");
+
+var config = {  
+    entry: DEV + "/index.jsx",
+      output: {     path: OUTPUT,     filename: "myCode.js"   }
+};
